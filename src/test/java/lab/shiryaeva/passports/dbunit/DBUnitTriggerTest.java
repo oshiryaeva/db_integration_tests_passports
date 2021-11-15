865d8e9742e4238f4db19ed0c2aae1c018f106bf
oshiryaeva <gospellcheck@gmail.com>
2021-11-15 12:06:54 +0400

    improve DBUnit tests

--- src/test/java/lab/shiryaeva/passports/dbunit/DBUnitTriggerTest.java
+++ src/test/java/lab/shiryaeva/passports/dbunit/DBUnitTriggerTest.java
@@ -1,24 +1,23 @@
 package lab.shiryaeva.passports.dbunit;
 
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import lab.shiryaeva.passports.PassportsApplication;
-import lab.shiryaeva.passports.model.Person;
 import lab.shiryaeva.passports.repository.Person2PassportRepository;
 import lab.shiryaeva.passports.service.PassportService;
 import lab.shiryaeva.passports.service.PersonService;
+import org.dbunit.Assertion;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.flywaydb.test.FlywayTestExecutionListener;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
@@ -26,15 +25,11 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 @SpringBootTest(classes = PassportsApplication.class)
 @FlywayTest
-@AutoConfigureTestEntityManager
 @ContextConfiguration(initializers = {DBUnitTriggerTest.Initializer.class})
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
         FlywayTestExecutionListener.class})
@@ -46,11 +41,10 @@ public class DBUnitTriggerTest {
     private PassportService passportService;
     @Autowired
     private Person2PassportRepository person2PassportRepository;
-    @Autowired
-    private EntityManager entityManager;
+
 
     @Container
-    public static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13")
+    static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13")
             .withDatabaseName("passports-test")
             .withUsername("postgres")
             .withPassword("password");
@@ -72,34 +66,44 @@ public class DBUnitTriggerTest {
 
     @Test
     @FlywayTest
-    @DatabaseSetup("person.xml")
-    @ExpectedDatabase("person_no_roosevelt.xml")
-    public void shouldRemovePersonFromDb() {
-        List<Person> personList = this.personService.getAllPersons();
-        assertEquals("Theodore", personList.get(25).getFirstName());
-        assertEquals("Roosevelt", personList.get(25).getLastName());
-        personService.delete(26);
-        assertEquals(45, personService.getAllPersons().size());
+    public void shouldRemovePersonFromDb() throws Exception {
+        IDatabaseTester tester = new JdbcDatabaseTester(postgreSQLContainer.getDriverClassName(),
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword());
+        IDataSet expectedData = new FlatXmlDataSetBuilder().build(
+                Thread.currentThread().getContextClassLoader()
+                        .getResourceAsStream("dataset/person.xml"));
+        String[] ignore = {"id", "birth_date"};
+        IDataSet actualData = tester.getConnection().createDataSet();
+        Assertion.assertEqualsIgnoreCols(expectedData, actualData, "person", ignore);
     }
 
     @Test
     @FlywayTest
-    @DatabaseSetup("passport.xml")
-    @ExpectedDatabase("passport_no_roosevelt.xml")
-    public void shouldRemovePersonAndLinkedPassportsFromDb() {
+    public void shouldRemovePersonAndLinkedPassportsFromDb() throws Exception {
+        IDatabaseTester tester = new JdbcDatabaseTester(postgreSQLContainer.getDriverClassName(),
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword());
+        IDataSet expectedData = new FlatXmlDataSetBuilder().build(
+                Thread.currentThread().getContextClassLoader()
+                        .getResourceAsStream("dataset/passport_no_roosevelt.xml"));
+        String[] ignore = {"id", "issue_date", "expiration_date"};
         personService.delete(26);
-        assertEquals(45, personService.getAllPersons().size());
-        assertEquals(96, passportService.getAllPassports().size());
+        IDataSet actualData = tester.getConnection().createDataSet();
+        Assertion.assertEqualsIgnoreCols(expectedData, actualData, "passport", ignore);
     }
 
+    // DBUnit cannot treat View as Table directly
     @Test
     @FlywayTest
-    @Modifying
-    @Query(value = "DELETE FROM person WHERE person.id=43", nativeQuery = true)
-    @DatabaseSetup("view.xml")
-    @ExpectedDatabase("view_no_bush.xml")
-    public void shouldUpdateViewAfterPersonDelete() {
-        personService.delete(43);
+    public void shouldUpdateViewAfterPersonDelete() throws Exception {
+        IDataSet expectedData = new FlatXmlDataSetBuilder().build(
+                Thread.currentThread().getContextClassLoader()
+                        .getResourceAsStream("dataset/view_no_biden.xml"));
+        personService.delete(46);
+        assertEquals(expectedData.getTable("view").getRowCount(), person2PassportRepository.findAll().size());
     }
 
     static class Initializer
